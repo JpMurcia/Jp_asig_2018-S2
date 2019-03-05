fa592833fb1f83dca6e84694b82f675f944ca033
JpMurcia <juan.murcia@udla.edu.co>
2019-03-04 21:15:45 -0500

    fdsafdsf
    fsdfasdf

---
+++ Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using proyectoweb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace proyectoweb.Views.ViewsUser
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        indexController contorlador = new indexController();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            llenar();
+        }
+
+        //public DataTable cosultarA()
+        //{
+
+        //    return activy.consul_acti(act);
+        //}
+
+        public void llenar() {
+            DataTable dt = contorlador.dato_pag();
+            descripcion.Text = dt.Rows[0]["Contenido"].ToString();
+           
+            //dt.Rows[0]["Nom_grupo"].ToString();
+            //dt.Rows[0]["Creadores"].ToString();
+
+        }
+    }
+}
